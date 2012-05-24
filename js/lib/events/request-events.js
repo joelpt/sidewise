a34895008cd096f2358a95586dd9a2c53a19f1c5
Joel Thornton <github@joelpt.net>
2012-05-24 11:43:40 -0700

    Don't assign a tab as its own child in onGetPageDetailsMessage().

--- js/lib/events/request-events.js
+++ js/lib/events/request-events.js
@@ -82,15 +82,17 @@ function onGetPageDetailsMessage(tab, msg)
     if (page === undefined)
     {
         // page entry doesn't exist anymore
-        throw 'pagedetails could not find page ' + tab.id + ' in the tree?!';
+        throw 'pagedetails could not find page ' + tab.id + ' in the tree';
     }
     // look for an existing tab whose url matches our tab's referrer
     chrome.tabs.query({ 'windowId': windowId, 'url': dropUrlHash(msg.referrer) }, function(tabs) {
         tree.updatePage(tabId, { placed: true });  // tab's proper tree location is now 'known'
-        if (tabs.length == 0)
+        // exclude potential parent candidates which are the same tab
+        tabs = tabs.filter(function(t) { return t.id != tabId; });
+        if (tabs.length == 0) {
             return;  // no apparent parent tab, so it belongs where it is under a window
+        }
         // TODO handle these cases:
-        //      parent tab == child tab
         //      parent tab is already a descendant of the tab with tabId
         log('making ' + tabId + ' a child of ' + tabs[0].id);
         tree.move('p' + tabId, 'p' + tabs[0].id);
