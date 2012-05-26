ec84c1dc4d68fccaa0cc7abfd60aff957ffdf314
Joel Thornton <github@joelpt.net>
2012-05-26 14:00:54 -0700

    Refine browser action behaviors; "show" mode now redocks sidebar to current window if needed

--- js/lib/events/browseraction-events.js
+++ js/lib/events/browseraction-events.js
@@ -18,16 +18,34 @@ function onBrowserActionClicked()
 
     // // No sidebar exists so just create it now
     // sidebarHandler.createDockedToCurrentWin(newSide);
-    var action = loadSetting('browserActionButtonBehavior', 'toggle');
+    var action = loadSetting('browserActionButtonBehavior');
 
     if (sidebarHandler.sidebarExists()) {
         if (action == 'toggle') {
+            // sidebar exists, so remove it
             sidebarHandler.remove();
             return;
         }
+
+        // sidebar exists, is it undocked?
+        if (sidebarHandler.dockState == 'undocked') {
+            // it's undocked so just raise it
+            chrome.windows.update(sidebarHandler.windowId, { focused: true });
+            return;
+        }
+
+        // sidebar exists, are we changing its dock target?
+        if (sidebarHandler.dockWindowId != focusTracker.getFocused()) {
+            // changing dock target
+            sidebarHandler.redock(focusTracker.getFocused());
+            return;
+        }
+
+        // sidebar exists and it's docked to the current focused window, so just raise the sidebar
         chrome.windows.update(sidebarHandler.windowId, { focused: true });
         return;
     }
 
-    sidebarHandler.createWithDockState(loadSetting('dockState', 'right'));
+    // sidebar doesn't exist so create it with user's choice of docking mode from settings
+    sidebarHandler.createWithDockState(loadSetting('dockState'));
 }
