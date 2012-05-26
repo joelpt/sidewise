8a9af6f6fb8d0003ae4736ba5de7d5bdb0252a39
Joel Thornton <github@joelpt.net>
2012-05-26 14:00:02 -0700

    Remove code obsoleted by ChromeWindowFocusTracker

--- js/lib/util/chrome-functions.js
+++ js/lib/util/chrome-functions.js
@@ -1,76 +1,35 @@
 /* Functions relating to Chrome specific functionality */
 
 // Get extension version from manifest.json.
- function getVersion() {
+function getVersion() {
     var details = chrome.app.getDetails();
     return details.version;
-  }
-
-// Used to track the current or last focused Chrome window, and whether any Chrome window
-// currently has focus
-var lastFocusedChromeWindowId = null;
-var focusedChromeWindowId = null;
-var chromeWindowIsFocused = false;
-
-
-function setChromeWindowIsFocused(isFocused) {
-    chromeWindowIsFocused = isFocused;
 }
 
-function setFocusedChromeWindowId(windowId) {
-    if (windowId == focusedChromeWindowId) {
-        // only want to record changes to focused win id's
-        return;
-    }
-    lastFocusedChromeWindowId = focusedChromeWindowId;
-    focusedChromeWindowId = windowId;
-    log(windowId);
+// Update window's position and state.
+// Pass null for metrics to leave as-is.
+function positionWindow(winId, state, left, top, width, height)
+{
+    var metrics = {};
+    if (state != null)
+        metrics.state = state;
+    if (left != null)
+        metrics.left = left;
+    if (top != null)
+        metrics.top = top;
+    if (width != null)
+        metrics.width = width;
+    if (height != null)
+        metrics.height = height;
+
+    chrome.windows.update(winId, metrics);
 }
 
-// function setFocusedChromeWindowId(windowId, callback) {
-//     if (windowId == focusedChromeWindowId) {
-//         // only want to record changes to focused win id's
-//         callback(false);
-//         return;
-//     }
-//     chrome.tabs.query({ active: true, windowId: windowId}, function(tabs) {
-//         if (isExtensionUrl(tabs[0].url)) {
-//             // don't record our own extension's tabs
-//             callback(false);
-//             return;
-//         }
-
-//     lastFocusedChromeWindowId = focusedChromeWindowId;
-//     focusedChromeWindowId = windowId;
-// }
-
-function isChromeWindowFocused() {
-    return chromeWindowIsFocused;
-}
-
-function getFocusedChromeWindowId() {
-    return focusedChromeWindowId;
-}
-
-function getLastFocusedChromeWindowId() {
-    return lastFocusedChromeWindowId;
-}
-
-/* Focus current window's tab in the page tree.
- * If focusLast is true, focus the *previously focused* window's tab instead.
- */
-function focusCurrentTabInPageTree(focusLast) {
-    var windowId = (focusLast ? getLastFocusedChromeWindowId() : getFocusedChromeWindowId());
+/* Focus the current Chrome window's active tab in the page tree. */
+function focusCurrentTabInPageTree() {
+    var windowId = focusTracker.getFocused();
     chrome.tabs.query({ active: true, windowId: windowId }, function(tabs) {
         var activeTab = tabs[0];
         tree.focusPage(activeTab.id);
-    });
-}
-
-function initializeFocusedChromeWindow(callback) {
-    chrome.windows.getLastFocused(null, function(win) {
-        setFocusedChromeWindowId(win.id);
-        setChromeWindowIsFocused(true);
-        callback(win);
     });
 }
