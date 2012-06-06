5b09ffa493c70125a10931ba3c9a7b787d7762ba
Joel Thornton <github@joelpt.net>
2012-06-06 11:42:41 -0700

    Migrate external-site.html's JS and CSS into separate files. Use TimeoutManager to manage the iframe-loading-fail timer.

---
+++ js/ui/sidebars/external-site.js
@@ -0,0 +1,27 @@
+var IFRAME_LOAD_TIMEOUT_MS = 12000;
+
+window.onload = onLoad;
+
+function onLoad() {
+    document.getElementById('contentFrame').onload=onLoadIframe;
+    setIframeSrc();
+}
+
+function onLoadIframe() {
+    document.getElementById('loadingHint').style.display = 'none';
+    TimeoutManager.clear('iframeLoad');
+}
+
+function setIframeSrc() {
+    var src = location.hash.slice(1);
+    document.getElementById('loadingHint').style.display = 'block';
+    document.getElementById('loadingURL').innerText = src;
+    document.getElementById('contentFrame').src = src;
+    TimeoutManager.set('iframeLoad', onIframeLoadTimeout, IFRAME_LOAD_TIMEOUT_MS);
+}
+
+function onIframeLoadTimeout() {
+    this.document.getElementById('loadingError').innerHTML =
+        'This content is taking a long time to load.<br/><br/>Many sites block being loaded in an IFRAME. ' +
+        'This may be the case here. If the content never loads, you are probably out of luck. Sorry!';
+}
