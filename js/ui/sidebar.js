2e8a8c2067e44f8d7989d0301898c2c62715bf9a
Joel Thornton <github@joelpt.net>
2012-06-09 16:09:32 -0700

    Permanently fix bug where user could sometimes cause sidebar panes to scroll unintentionally. Avoid passing openerTabId when opening options.html from options button. Refactor event handler functions into non-anonymous functions.

--- js/ui/sidebar.js
+++ js/ui/sidebar.js
@@ -1,9 +1,13 @@
+//////////////////////////////////////////
+// Initialization
+//////////////////////////////////////////
+
 var sidebars = [
     { id: 'pages', url: 'sidebars/pages.html', label: 'Pages', icon: 'images/nav/pages.png' },
-    { id: 'reddit', url: 'sidebars/external-site.html#http://i.reddit.com', label: 'Reddit', icon: 'images/nav/reddit.png' },
-    { id: 'tribunal', url: 'sidebars/external-site.html#https://tribunal.herokuapp.com/', label: 'Tribunal', icon: 'images/nav/tribunal.png' },
-    { id: 'twitter', url: 'sidebars/external-site.html#http://mobile.twitter.com', label: 'Twitter', icon: 'http://twitter.com/favicon.ico' },
-    { id: 'notepad', url: 'sidebars/notepad.html', label: 'Notepad', icon: 'images/nav/notepad.png' }
+    { id: 'notepad', url: 'sidebars/notepad.html', label: 'Notepad', icon: 'images/nav/notepad.png' },
+    { id: 'reddit', url: 'sidebars/external-site.html#http://i.reddit.com', label: 'Reddit', icon: 'images/nav/reddit.png' }//,
+    // { id: 'tribunal', url: 'sidebars/external-site.html#https://tribunal.herokuapp.com/', label: 'Tribunal', icon: 'images/nav/tribunal.png' },
+    // { id: 'twitter', url: 'sidebars/external-site.html#http://mobile.twitter.com', label: 'Twitter', icon: 'http://twitter.com/favicon.ico' },
 ];
 
 var initialSidebar = 'pages';
@@ -16,8 +20,6 @@ function onReady() {
     bg = chrome.extension.getBackgroundPage();
     manager = new SidebarNavManager($('ul#sidebarButtons'), $('tr#sidebars'),
         $('table#main'), $('body'), 'td');
-    // manager.createSidebarButtons();
-    // manager.createSidebarContainers();
     manager.addSidebarPanels(sidebars);
 
     // Set initial sidebar position
@@ -30,22 +32,9 @@ function onReady() {
         }, 0);
     });
 
-    $(document).keydown({manager: manager}, function(evt) {
-        if (evt.keyCode == 70 && evt.ctrlKey) {
-            var iframe = $(
-                $('#' + evt.data.manager.currentSidebar).children('iframe').get(0).contentWindow.document
-            );
-            var filterBox = iframe.find('#pageFilter');
-            if (filterBox.length == 0) {
-                filterBox.focus();
-            }
-            evt.stopPropagation();
-            return false;
-        }
-        return true;
-    });
-
-    $(window).resize(onResize);
+    $(document).keydown(onDocumentKeyDown);
+    $(document).scroll(onDocumentScroll);
+    $(window).resize(onWindowResize);
 
     $('#optionsButton')
         .attr('title', getMessage('sidebars_optionsButtonTooltip'))
@@ -55,17 +44,49 @@ function onReady() {
     setI18NText();
 }
 
+
+//////////////////////////////////////////
+// Event handlers
+//////////////////////////////////////////
+
+function onDocumentKeyDown(evt) {
+    // focus filter box in child pane if present
+    if (evt.keyCode == 70 && evt.ctrlKey) {
+        var iframe = $(
+            $('#' + evt.data.manager.currentSidebar).children('iframe').get(0).contentWindow.document
+        );
+        var filterBox = iframe.find('#pageFilter');
+        if (filterBox.length == 0) {
+            filterBox.focus();
+        }
+        evt.stopPropagation();
+        return false;
+    }
+    return true;
+}
+
+function onDocumentScroll() {
+    // prevent user scrolling of sidebar panes through e.g. drag-selecting some text
+    // and moving the mouse off the edge of the sidebar window
+    manager.scrollToCurrentSidebarPanel(true);
+}
+
+function onWindowResize() {
+    // perform resize work, e.g. resizing an attached dock window
+    bg.sidebarHandler.onWindowResize();
+
+    // prevent width-resizing of sidebar from showing part of another sidebar pane
+    manager.scrollToCurrentSidebarPanel(true);
+}
+
 function onClickOptionsButton() {
-    chrome.tabs.query({ url: chrome.extension.getURL('/options.html') }, function(tabs) {
+    var optionsUrl = chrome.extension.getURL('/options.html');
+    chrome.tabs.query({ url: optionsUrl }, function(tabs) {
         if (tabs.length == 0) {
-            window.open('options.html');
+            chrome.tabs.create({ url: optionsUrl });
             return;
         }
         chrome.tabs.update(tabs[0].id, { active: true });
     });
 }
 
-function onResize() {
-    bg.sidebarHandler.onResize();
-    manager.scrollToCurrentSidebarPanel(true);
-}
