5f5caf3da7b7e2215640064eba1388c376dfe2ac
Joel Thornton <github@joelpt.net>
2012-06-18 12:05:20 -0700

    Add markdown transformation to messages.json messages with start with '#' or '!#'. Transform // into \n before doing markdown transforms. Transform // into \n\n for messages that have a key of 'prompt_*' (those that will be in confirm()/alert()/input() dialog boxes).

--- js/util/i18n.js
+++ js/util/i18n.js
@@ -8,7 +8,33 @@ function setI18NText() {
     });
 }
 
-// Get an i18n message from _locales/xx/messages.json
+// Get an i18n message from _locales/xx/messages.json and transform it
 function getMessage(key, placeholderValues) {
-    return chrome.i18n.getMessage(key, placeholderValues);
+    var msg = chrome.i18n.getMessage(key, placeholderValues);
+    if (key.match(/^prompt_/)) {
+        return transformPromptMessage(msg);
+    }
+    if (msg.match(/^#/)) {
+        // leading # tells us we want to do markdown syntax transformation
+        return transformMessage(msg.slice(1));
+    }
+    if (msg.match(/^!#/)) {
+        // leading # tells us we want to do markdown syntax transformation,
+        // and leading ! is used to denote "warning" hints in options, so preserve
+        // the ! after transformation
+        return '!' + transformMessage(msg.slice(2));
+    }
+    // just return the message untransformed
+    return msg;
+}
+
+// Transform a message using Markdown syntax translation after turning //'s into \n's
+function transformMessage(msg) {
+    msg = msg.replace(/\s*\/\/\s*/g, '\n');
+    return marked(msg);
+}
+
+// Transform a prompt message, just turning //'s into \n\n
+function transformPromptMessage(msg) {
+    return msg.replace(/\s*\/\/\s*/g, '\n\n');
 }
