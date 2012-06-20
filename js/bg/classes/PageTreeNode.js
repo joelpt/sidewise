ad9c023da4cb90f2aa979860b644662678c380c9
Joel Thornton <github@joelpt.net>
2012-06-20 14:59:04 -0700

    Add highlighted, hibernated, restorable properties to PageTreeNode.

--- js/bg/classes/PageTreeNode.js
+++ js/bg/classes/PageTreeNode.js
@@ -6,7 +6,10 @@ var PageTreeNode = function()
 {
     PageTreeNode._base.call(this);
     this.label = '';
+    this.highlighted = false;
     this.collapsed = false;
+    this.hibernated = false;
+    this.restorable = false;
 };
 
 PageTreeNode.extend(DataTreeNode);
