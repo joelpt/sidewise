2bb85cb95a9ad9566f1da81fd0488b6ebffe9809
Joel Thornton <github@joelpt.net>
2012-07-02 11:50:03 -0700

    Add $.reverse() jQuery helper function.

--- js/jquery/jquery-helpers.js
+++ js/jquery/jquery-helpers.js
@@ -1,3 +1,6 @@
+// Add $.reverse()
+jQuery.fn.reverse = [].reverse;
+
 // Adds an :icontains jQuery selector, which does a case-insensitive match of elements' contained text
 $.expr[':'].icontains = function(obj, index, meta, stack){
     return (obj.textContent || obj.innerText || jQuery(obj).text() || '')
