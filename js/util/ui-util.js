6a747785469707705f94202667e4a9efbe661957
Joel Thornton <github@joelpt.net>
2012-07-17 09:33:43 -0700

    Add copyTextToClipboard().

--- js/util/ui-util.js
+++ js/util/ui-util.js
@@ -12,3 +12,17 @@ function isScrolledIntoView(elem)
     return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom)
       && (elemBottom <= docViewBottom) &&  (elemTop >= docViewTop) );
 }
+
+function copyTextToClipboard(text) {
+    var copyFrom = $('<textarea/>', {
+        style: 'height: 0px; width: 0px; position: fixed; left: -10000px; top: -10000px;',
+        id: 'copyFrom',
+        cols: 60,
+        rows: 5
+    });
+    copyFrom.text(text);
+    $('body').append(copyFrom);
+    copyFrom.select();
+    document.execCommand('copy');
+    copyFrom.remove();
+}
