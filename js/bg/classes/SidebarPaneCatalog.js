9c3e71289dd4c02c58b8816863133ad0148dbb03
Joel Thornton <github@joelpt.net>
2012-10-18 10:32:38 -0700

    Don't show reddit pane by default.

--- js/bg/classes/SidebarPaneCatalog.js
+++ js/bg/classes/SidebarPaneCatalog.js
@@ -1,7 +1,7 @@
 var AVAILABLE_PANES = [
     { enabled: true, id: 'pages', url: 'sidebars/pages.html', label: getMessage('sidebarLabel_Pages'), icon: 'images/nav/pages.png' },
     { enabled: true, id: 'notepad', url: 'sidebars/notepad.html', label: getMessage('sidebarLabel_Notepad'), icon: 'images/nav/notepad.png' },
-    { enabled: true, id: 'reddit', url: 'sidebars/external-site.html#http://i.reddit.com', label: 'Reddit', icon: 'images/nav/reddit.png' },
+    { enabled: false, id: 'reddit', url: 'sidebars/external-site.html#http://i.reddit.com', label: 'Reddit', icon: 'images/nav/reddit.png' },
     { enabled: false, id: 'grooveshark', url: 'sidebars/external-site.html#http://html5.grooveshark.com/#!/music/stations', label: 'Grooveshark', icon: 'images/nav/grooveshark.ico' }
 ];
 
