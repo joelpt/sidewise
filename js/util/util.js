b4944b2417eccc2cbfa5a346cef8eb94923daad0
Joel Thornton <github@joelpt.net>
2012-12-20 11:29:11 -0800

    Add daysBetween().

--- js/util/util.js
+++ js/util/util.js
@@ -484,6 +484,21 @@ function formatSecondsAsHMS(seconds)
     return '[' + hours + mins + ':' + secs + ']';
 }
 
+function daysBetween(date1, date2) {
+
+    // The number of milliseconds in one day
+    var ONE_DAY = 1000 * 60 * 60 * 24;
+
+    // Convert both dates to milliseconds
+    var date1_ms = date1.getTime();
+    var date2_ms = date2.getTime();
+
+    // Calculate the difference in milliseconds
+    var difference_ms = Math.abs(date1_ms - date2_ms);
+
+    // Convert back to days and return
+    return Math.round(difference_ms/ONE_DAY);
+}
 
 ///////////////////////////////////////////////////////////
 // GUID generation
