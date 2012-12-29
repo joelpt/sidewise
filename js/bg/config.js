b70726046ae88d616ec29daf9a001d79922cb17b
Joel Thornton <github@joelpt.net>
2012-12-28 18:43:47 -0800

    File reorganization.

--- js/bg/config.js
+++ js/bg/config.js
@@ -1,11 +1,11 @@
 var config = config || {};
 
 config.AVAILABLE_PANES = [
-    { enabled: true, id: 'pages', url: 'sidebars/pages.html', label: getMessage('sidebarLabel_Pages'), icon: 'images/nav/pages.png' },
-    { enabled: true, id: 'closed', url: 'sidebars/closed.html', label: 'Recently closed', icon: 'images/nav/closed.png' },
-    { enabled: true, id: 'notepad', url: 'sidebars/notepad.html', label: getMessage('sidebarLabel_Notepad'), icon: 'images/nav/notepad.png' },
-    { enabled: false, id: 'reddit', url: 'sidebars/external-site.html#http://i.reddit.com', label: 'Reddit', icon: 'images/nav/reddit.png' },
-    { enabled: false, id: 'grooveshark', url: 'sidebars/external-site.html#http://html5.grooveshark.com/#!/music/stations', label: 'Grooveshark', icon: 'images/nav/grooveshark.ico' }
+    { enabled: true, id: 'pages', url: 'panes/pages.html', label: getMessage('sidebarLabel_Pages'), icon: 'images/nav/pages.png' },
+    { enabled: true, id: 'closed', url: 'panes/closed.html', label: 'Recently closed', icon: 'images/nav/closed.png' },
+    { enabled: true, id: 'notepad', url: 'panes/notepad.html', label: getMessage('sidebarLabel_Notepad'), icon: 'images/nav/notepad.png' },
+    { enabled: false, id: 'reddit', url: 'panes/external-site.html#http://i.reddit.com', label: 'Reddit', icon: 'images/nav/reddit.png' },
+    { enabled: false, id: 'grooveshark', url: 'panes/external-site.html#http://html5.grooveshark.com/#!/music/stations', label: 'Grooveshark', icon: 'images/nav/grooveshark.ico' }
 ];
 
 config.TREE_ONMODIFIED_DELAY_ON_STARTUP_MS = 1500;
