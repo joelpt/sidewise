fbb8538d341d293d1311c7f0edd5000fc3b94373
Joel Thornton <github@joelpt.net>
2013-01-04 12:25:09 -0800

    Add IconTester().

--- js/bg/background.js
+++ js/bg/background.js
@@ -897,3 +897,194 @@ function showPromoPageAnnually() {
     }
 }
 
+
+var preventTestIconsCheck;
+var IconTester = function() {
+    this.testTab = undefined;
+    this.testDomWindow = undefined;
+    this.testResetTime = undefined;
+    this.testOnFinished = undefined;
+};
+
+IconTester.prototype = {
+    testIcons: function() {
+        preventTestIconsCheck = true;
+        var msg = 'Sidewise\'s sidebar appears to be showing a Chrome malware warning page. This can happen when you visit a ' +
+            'page which Chrome believes to contain malware. Because Sidewise shows the favicon of such pages in the sidebar, ' +
+            'this also triggers Chrome\'s malware warning within the sidebar itself.\n\n' +
+            'Sidewise can try to fix this by identifying the bad favicon and removing it from the sidebar.\n\n' +
+            'This process will take about a minute. DO NOT interact with the machine until it is complete.';
+        if (!confirm(msg)) {
+            alert('You declined to do the favicon malware test. Sidewise won\'t ask again until you restart Chrome.');
+            return;
+        }
+
+        this.testTab = undefined;
+        this.testDomWindow = undefined;
+        this.testOnFinished = undefined;
+
+        try {
+            if (sidebarHandler.sidebarExists()) {
+                sidebarHandler.remove();
+            }
+            this.doTestIcons(this.onTestIconsFinished);
+        }
+        catch (ex) {
+            if (this.testTab) {
+                this.destroyTestIconsPage(this.testTab.id);
+            }
+            alert('Sorry, but something went wrong during the testing process. No changes have been made to your tree.');
+        }
+    },
+
+    onTestIconsFinished: function(badNodes) {
+        this.destroyTestIconsPage();
+        if (badNodes.length > 0) {
+            badNodes.forEach(function(e) {
+                e.favicon = 'chrome://favicon';
+            });
+            savePageTreeToLocalStorage(tree, 'pageTree', true);
+            savePageTreeToLocalStorage(recentlyClosedTree, 'recentlyClosedTree', true);
+        }
+
+        setTimeout(function() {
+            sidebarHandler.createWithDockState(settings.get('dockState'));
+            if (badNodes.length == 0) {
+                alert('Sidewise did not find any favicons that caused the malware page problem. Sorry!\n\n' +
+                    'If you are still seeing the malware page in the sidebar, restarting Chrome and rerunning this test will usually fix it.');
+                return;
+            }
+            alert('The testing process is complete and Sidewise has detected ' + badNodes.length + ' favicon(s) that cause the malware page problem.' +
+                '\n\nSidewise has removed these favicons from the sidebar and the problem should now be resolved.');
+            preventTestIconsCheck = false;
+        }, 500);
+    },
+
+    doTestIcons: function(onFinished) {
+        this.testTab = undefined;
+        this.testDomWindow = undefined;
+        this.testOnFinished = onFinished;
+        sidebarHandler.sidebarPanes['test_icons'] = undefined;
+        this.startTestIconsLoops();
+    },
+
+    createTestIconsPage: function(onCreated) {
+        var self = this;
+        chrome.tabs.create({ url: 'test_icons.html' }, function(tab) {
+            self.onTestIconsTabCreated(tab, onCreated);
+        });
+    },
+
+    resetTestIconsPage: function(onResetDone) {
+        var self = this;
+        if (this.testTab) {
+            this.destroyTestIconsPage(function() {
+                self.createTestIconsPage(onResetDone);
+            });
+            return;
+        }
+        this.createTestIconsPage(onResetDone);
+    },
+
+    destroyTestIconsPage: function(onDestroyed) {
+        var self = this;
+        if (this.testTab) {
+            chrome.tabs.remove(this.testTab.id, function() {
+                self.testTab = undefined;
+                self.testDomWindow = undefined;
+                onDestroyed(true);
+            });
+            return;
+        }
+        onDestroyed(false);
+    },
+
+    onTestIconsTabCreated: function(tab, onReady) {
+        var self = this;
+        if (!tab) {
+            throw new Error('Test icons tab failed to load.');
+        }
+        this.testTab = tab;
+        setTimeout(function() {
+            var domWindow = sidebarHandler.sidebarPanes['test_icons'];
+            if (!domWindow) {
+                self.onTestIconsTabCreated(tab, onReady);
+                return;
+            }
+            self.testDomWindow = domWindow;
+            onReady();
+        }, 500);
+    },
+
+    startTestIconsLoops: function() {
+        var self = this;
+        var badNodes = [];
+        this.testIconsInTree(tree, function(badNode) {
+            if (badNode) {
+                console.warn('GOT IT', badNode.id, badNode.favicon);
+                badNodes.push(badNode);
+            }
+            self.testIconsInTree(recentlyClosedTree, function(badNode) {
+                if (badNode) {
+                    console.warn('RC GOT IT', badNode.id, badNode.favicon);
+                    badNodes.push(badNode);
+                }
+                self.testOnFinished(badNodes);
+            });
+        });
+    },
+
+    testIconsInTree: function(tree, onFinished) {
+        var nodes = tree.filter(function(e) {
+            return e instanceof PageNode && e.favicon;
+        });
+        this.testResetTime = 5000;
+        this.testIconBatch(nodes, function() {
+            onFinished();
+        }, function(badNode) {
+            onFinished(badNode);
+        });
+    },
+
+    testIconBatch: function(nodes, onAllValid, onFoundInvalid) {
+        var self = this;
+        this.resetTestIconsPage(function() {
+            for (var i = 0; i < nodes.length; i++) {
+                var node = nodes[i];
+                self.testDomWindow.testIcon(node.favicon);
+            };
+            setTimeout(function() {
+                chrome.tabs.get(self.testTab.id, function(t) {
+                    if (t.title.toLowerCase().indexOf('malware') >= 0) {
+                        // one of the tested icons had a problem
+                        if (nodes.length == 1) {
+                            // found the bad icon
+                            onFoundInvalid(nodes[0]);
+                            return;
+                        }
+                        // more than one tested icon had a problem,
+                        // so split the test batch in half and test
+                        // each half separately
+                        var batch1 = nodes.slice(0, nodes.length / 2);
+                        var batch2 = nodes.slice(nodes.length / 2);
+
+                        self.testResetTime = 2000;
+                        self.testIconBatch(batch1, function() {
+                            // all the icons in the first batch are valid
+                            // so test the second batch
+                            self.testIconBatch(batch2, function() {
+                                // all the icons in the second batch are valid
+                                // but this should be impossible
+                                throw new Error('Found a bad icon in an earlier iteration but did not find it in subdivision process!');
+                            }, onFoundInvalid);
+                        }, onFoundInvalid);
+                    }
+                    else {
+                        // no icon came up on error
+                        onAllValid();
+                    }
+                });
+            }, this.testResetTime);
+        });
+    }
+};
